97fbfe83d05b5bb80f29c2ff27f3963b80567b6b
Vml11 <victorbcn98@gmail.com>
2020-03-16 21:31:57 +0100

    Added pathologies table related to user

--- src/main/resources/sql/V0_0_1__init.sql
+++ src/main/resources/sql/V0_0_1__init.sql
@@ -12,6 +12,19 @@ create table daily_answer(
                              creation_datetime timestamp not null default CURRENT_TIMESTAMP
 );
 
+create table pathologies(
+    app_user_id char(16) not null,
+    pathology varchar(191),
+    primary key (app_user_id, pathology),
+    unique (app_user_id, pathology)
+);
+
+alter table pathologies
+    add constraint pathologies_user_fk
+        foreign key(app_user_id)
+        references app_user(id)
+        on delete cascade;
+
 alter table daily_answer
     add constraint daily_answer_user_fk
         foreign key (app_user_id)
